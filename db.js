0c4311b701bd3e9876b678c3279652dffe0f20cf
Pratik Mohanty <pratik@froker.in>
2024-08-09 20:32:47 +0530

    changes

--- db.js
+++ db.js
@@ -1,22 +1,29 @@
+
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/user', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-console.log("db")
+const MONGODB_URI =  'mongodb+srv://pratik:Pkmty!23@cluster0.zo20xjv.mongodb.net/?retryWrites=true&w=majority&';
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB successfully");
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:", error);
+  });
+
 const postbackSchema = new mongoose.Schema({
-    click: String,
-    idfa: String,
-    aaid: String,
-    user_agent: String,
-    os: String,
-    os_version: String,
-    app_version: String,
-    country: String,
-    event: String,
-    timestamp: { type: Date, default: Date.now }
+  click: String,
+  idfa: String,
+  aaid: String,
+  user_agent: String,
+  os: String,
+  os_version: String,
+  app_version: String,
+  country: String,
+  event: String,
+  timestamp: { type: Date, default: Date.now }
 });
+
 const Postback = mongoose.model('Postback', postbackSchema);
 
-module.exports =Postback ;
+module.exports = Postback;
